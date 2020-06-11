d2eb85768d5dc703b275c8c6055bce7cbae84f70
Mateo Querede <mateuqarg@gmail.com>
2020-06-11 16:23:53 -0300

    Heroku

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -23,6 +23,6 @@ class MainController extends Controller
 
     	// $media = array_sum($numeros)/count($numeros);
 
-    	return view('home', compat('most_visited'));
+    	return view('home', compact('most_visited'));
     }
 }
